c0f26b9b24218d5f9efcd12a028568b083ad98bd
kasyap2001 <68432639+kasyap2001@users.noreply.github.com>
2020-07-18 12:51:03 +0530

    Update swapping numbers.c
    This program is written to perform the swapping of two numbers with out using temporary variables and after executing this program we will be able to perform swapping of two numbers

--- swapping numbers.c
+++ swapping numbers.c
@@ -1,23 +1,20 @@
+
 #include <stdio.h>
-int main() {
-    double a, b;
-    printf("Enter a: ");
-    scanf("%lf", &a);
-    printf("Enter b: ");
-    scanf("%lf", &b);
-
-    // Swapping
-
-    // a = (initial_a - initial_b)
-    a = a - b;   
- 
-    // b = (initial_a - initial_b) + initial_b = initial_a
-    b = a + b;
-
-    // a = initial_a - (initial_a - initial_b) = initial_b
-    a = b - a;
-
-    printf("After swapping, a = %.2lf\n", a);
-    printf("After swapping, b = %.2lf", b);
-    return 0;
+int main()
+{
+	int a,b;
+	printf("\n Enter the value of a: ");
+	scanf("%d",&a);
+	printf("\n Enter the value of b: ");
+	scanf("%d",&b);
+	
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	
+	printf("After swapping a: %d \n",a);
+	
+	printf("After swapping b: %d \n",b);
+	return 0;
+	
 }
